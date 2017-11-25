f5957a0ae8d82e5d7b4067e5aff95ec9f74a6dda
joelee2017 <as955133@gmail.com>
2017-11-25 15:33:02 +0800

    上傳

--- ConPoly/Program.cs
+++ ConPoly/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace ConPoly
 {
+    class Animal
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("Animal");
+        }        
+    }
+
+    class Person : Animal
+    {
+        public override void display()
+        {
+            Console.WriteLine("Person");
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Animal ani = new Animal();
+            Person per = new Person();
+            ani.display();
+            per.display();
+            Console.WriteLine("=========================");
+            ani = per;
+            ani.display();
+            Console.Read();
         }
     }
 }
